13f015e6cbb58ba22a1e8e76b2b9a212c2e15f02
janagamadeepika23 <janagamadeepika23@gmail.com>
2026-02-11 16:44:01 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,43 +1,58 @@
-import React from 'react'
- import './Footer.css'
+import React from 'react';
+import './Footer.css';
+
 import logo_big from "../../assets/logo_big.png";
- import instagram_icon from "../../assets/instagram.png";
-    import pintester_icon from "../../assets/pintester_icon.png";
-  
-     import whatsapp_icon from "../../assets/whatsapp_icon.png";
-  const Footer = () => { 
-    return (
-       <div className='footer'>
-         <div className='footer-logo'>
-            <img src={logo_big} alt=''/>
-          <p>SHOPPER</p> 
-          </div>
-          <div className='paragraph'>
-            <p> Shoppers can easily compare prices, read detailed product descriptions, 
-              and access reviews from previous customers across different websites,
-               helping them make more informed purchasing decisions.</p>
-          </div>
-          
-           <ul className='footer-links'>
-            
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
-          </ul>
-         
-                 <div className='footer-social-icon'> 
-                  <div className='footer-icons-container'>
-                     <img src={instagram_icon} alt=""/>
-                      </div> 
-                      <div className='footer-icons-container'> </div>
-                        <img src={pintester_icon} alt=""/>
-                       <div className='footer-icons-container'> </div>
-                         <img src={whatsapp_icon} alt=""/>
-                        </div> <div className='footer-copyright'>
-                           <hr/> 
-                           <p>Copyright @ 2025-All Right Reserved.</p>
-                            </div> 
-                            </div>
-                             ) }
- export default Footer
+import instagram_icon from "../../assets/instagram_icon.png";
+import pintester_icon from "../../assets/pintester_icon.png";
+import whatsapp_icon from "../../assets/whatsapp_icon.png";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+      <div className='footer-logo'>
+        <img src={logo_big} alt='logo'/>
+        <p>SHOPPER</p>
+      </div>
+
+      <div className='paragraph'>
+        <p>
+          Shoppers can easily compare prices, read detailed product descriptions,
+          and access reviews from previous customers across different websites,
+          helping them make more informed purchasing decisions.
+        </p>
+      </div>
+
+      <ul className='footer-links'>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Delivery</li>
+        <li>Privacy Policy</li>
+      </ul>
+
+      <div className='footer-social-icon'>
+
+        <div className='footer-icons-container'>
+          <img src={instagram_icon} alt="Instagram"/>
+        </div>
+
+        <div className='footer-icons-container'>
+          <img src={pintester_icon} alt="Pinterest"/>
+        </div>
+
+        <div className='footer-icons-container'>
+          <img src={whatsapp_icon} alt="WhatsApp"/>
+        </div>
+
+      </div>
+
+      <div className='footer-copyright'>
+        <hr/>
+        <p>Copyright © 2025 - All Rights Reserved.</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Footer;
